cd0d6c3ced7a60466caa75822f81a6a7c7e9c404
yamikaganji <86185074+yamikaganji@users.noreply.github.com>
2021-06-23 23:17:31 +0700

    java program to demonstrate localvariables

---
+++ Test1.java
@@ -0,0 +1,16 @@
+package basicjavaprograms;
+
+public class Test1 {
+	void age()
+	{
+		int age=0;
+		age=age+3;
+		System.out.println("age is:"+age);
+	}
+	public static void main(String args[])
+	{
+		Test1 t=new Test1();
+		t.age();
+	}
+
+}
